cc8afe25942556746a6be5e604866202f9b35aa0
d.ochkasov <d.ochkasov@qiwi.com>
2023-04-17 21:25:25 +0300

    init

--- src/test/java/com/demoqa/pageobjects/pages/components/CalendarComponent.java
+++ src/test/java/com/demoqa/pageobjects/pages/components/CalendarComponent.java
@@ -1,15 +1,19 @@
 package com.demoqa.pageobjects.pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static java.lang.String.format;
 
 public class CalendarComponent {
+    SelenideElement mouthSelect = $(".react-datepicker__month-select"),
+                    yearSelect = $(".react-datepicker__year-select");
 
     public void setDate(String day, String month, String year){
-        $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__month-select").selectOption(month);
-        String dayPickerLocator = format(".react-datepicker__day--0%s:not(.react-datapicker__day--outside-,month)", day);
-        $(dayPickerLocator).click();
+       mouthSelect.selectOption(month);
+       yearSelect.selectOption(year);
+       String dayPickerLocator = format(".react-datepicker__day--0%s:not(.react-datapicker__day--outside-,month)", day);
+       $(dayPickerLocator).click();
     }
 }
