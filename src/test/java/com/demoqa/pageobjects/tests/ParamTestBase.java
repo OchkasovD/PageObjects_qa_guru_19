21503c47661bd09ca1f2476840b9ea1b0ce71427
d.ochkasov <d.ochkasov@qiwi.com>
2023-05-23 21:51:20 +0300

    RandomUtils Enums FieldsView TextBoxTestGen Added get Remote Add Add Steps Fix remote tests add params jenkins

--- src/test/java/com/demoqa/pageobjects/tests/ParamTestBase.java
+++ src/test/java/com/demoqa/pageobjects/tests/ParamTestBase.java
@@ -15,7 +15,7 @@ public class ParamTestBase {
     static void beforeAll() {
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         String[] browserAndVersion = System.getProperty("browser", "chrome:100.0").split(":");
-        Configuration.browser = browserAndVersion[0];
+        Configuration.browser = browserAndVersion[1];
         Configuration.browserVersion = browserAndVersion[1];
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = "eager";
